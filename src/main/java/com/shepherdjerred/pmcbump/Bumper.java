139312c8c20ba9a7df8e5a9b3ee4893678234a87
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-02-14 22:18:15 -0600

    Finish login code

--- src/main/java/com/shepherdjerred/pmcbump/Bumper.java
+++ src/main/java/com/shepherdjerred/pmcbump/Bumper.java
@@ -4,6 +4,7 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Bumper {
@@ -28,28 +29,52 @@ public class Bumper {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("Server bumped");
     }
 
     private void login() throws IOException {
-        Connection.Response response = Jsoup.connect("http://www.planetminecraft.com/forums/ucp.php?mode=login_from_main")
+        Connection.Response loginForm = Jsoup.connect("http://www.planetminecraft.com/account/sign_in/")
+                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36")
+                .method(Connection.Method.GET)
+                .execute();
+
+        Connection.Response loginPost = Jsoup.connect("http://www.planetminecraft.com/forums/ucp.php?mode=login_from_main")
+                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36")
                 .data("username", email)
                 .data("password", password)
+                .data("login", "Sign In")
+                .data("autologin", "1")
+                .data("from_main_site", "1")
+                .cookies(loginForm.cookies())
                 .method(Connection.Method.POST)
                 .execute();
 
-        cookies = response.cookies();
-        System.out.println(cookies);
+        if (loginPost.body().contains("Sign Out")) {
+            System.out.println("Login success");
+        } else {
+            System.out.println("Login failure");
+        }
+
+        Map<String, String> combinedCookies = new HashMap<>();
+        combinedCookies.putAll(loginForm.cookies());
+        combinedCookies.putAll(loginPost.cookies());
+
+        cookies = combinedCookies;
     }
 
     private void bump() throws IOException {
 
-        Connection.Response response = Jsoup.connect("http://www.planetminecraft.com/qajax.php?resource_id=" + resourceId + "&member_id=" + memberId + "&action=bump")
+        System.out.println(cookies);
+
+        Connection.Response response = Jsoup.connect("http://www.planetminecraft.com/qajax.php")
+                .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36")
                 .cookies(cookies)
+                .data("resource_id", resourceId)
+                .data("member_id", memberId)
+                .data("action", "bump")
                 .method(Connection.Method.GET)
                 .execute();
 
-        System.out.println(response.body());
+        System.out.println("Bump: " + response.body());
 
     }
 
--- src/main/java/com/shepherdjerred/pmcbump/Main.java
+++ src/main/java/com/shepherdjerred/pmcbump/Main.java
@@ -3,7 +3,6 @@ package com.shepherdjerred.pmcbump;
 public class Main {
 
     public static void main(String args[]) {
-        System.out.println("Bumping server");
         Bumper bumper = new Bumper("jsheph032010@live.com", "password", "3004178", "209278");
         bumper.bumpServer();
     }
